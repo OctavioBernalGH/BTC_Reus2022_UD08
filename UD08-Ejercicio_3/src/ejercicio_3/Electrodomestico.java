67882084b9b970bb399cac07577c3910d6ab710c
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-04-13 19:10:33 +0200

    Modificaciones Ejercicio 3 y 4 UD08
    Se añade el autor, fecha y versión.

--- UD08-Ejercicio_3/src/ejercicio_3/Electrodomestico.java
+++ UD08-Ejercicio_3/src/ejercicio_3/Electrodomestico.java
@@ -1,30 +1,41 @@
 package ejercicio_3;
 
+/*
+ * @Autor:		Octavio Bernal Vilana
+ * @Fecha:		13/04/2022
+ * @Versión:	0.0.0.1
+ */
+
 public class Electrodomestico {
 
-	public enum Color { blanco, negro, rojo, azul, gris };
-	public enum Consumo { A, B, C, D, F };
-	
+	public enum Color {
+		blanco, negro, rojo, azul, gris
+	};
+
+	public enum Consumo {
+		A, B, C, D, F
+	};
+
 	private Color mycolor;
 	private Consumo myconsumo;
 	private double precioBase;
 	private double peso;
-	
+
 	public Electrodomestico() {
 		this.precioBase = 100;
 		this.mycolor = Color.blanco;
 		this.myconsumo = Consumo.F;
 		this.peso = 5;
-		
+
 	}
-	
+
 	public Electrodomestico(double precioBase, double peso) {
 		this.precioBase = precioBase;
 		this.mycolor = Color.blanco;
 		this.myconsumo = Consumo.F;
 		this.peso = peso;
 	}
-	
+
 	public Electrodomestico(double precioBase, Color mycolor, Consumo myconsumo, double peso) {
 		this.precioBase = precioBase;
 		this.mycolor = mycolor;
@@ -37,6 +48,5 @@ public class Electrodomestico {
 		return "Electrodomestico [precioBase=" + precioBase + ", mycolor=" + mycolor + ", myconsumo=" + myconsumo
 				+ ", peso=" + peso + "]";
 	}
-	
-	
+
 }
