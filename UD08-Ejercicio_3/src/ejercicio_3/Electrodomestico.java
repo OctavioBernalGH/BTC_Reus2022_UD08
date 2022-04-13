960617be367789e40c1e31d301dd97b89857b049
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-04-13 19:07:47 +0200

    Ejercicio 3 UD08.
    Se añade el ejercicio 3 de la UD08 a la rama desarrollo.

---
+++ UD08-Ejercicio_3/src/ejercicio_3/Electrodomestico.java
@@ -0,0 +1,42 @@
+package ejercicio_3;
+
+public class Electrodomestico {
+
+	public enum Color { blanco, negro, rojo, azul, gris };
+	public enum Consumo { A, B, C, D, F };
+	
+	private Color mycolor;
+	private Consumo myconsumo;
+	private double precioBase;
+	private double peso;
+	
+	public Electrodomestico() {
+		this.precioBase = 100;
+		this.mycolor = Color.blanco;
+		this.myconsumo = Consumo.F;
+		this.peso = 5;
+		
+	}
+	
+	public Electrodomestico(double precioBase, double peso) {
+		this.precioBase = precioBase;
+		this.mycolor = Color.blanco;
+		this.myconsumo = Consumo.F;
+		this.peso = peso;
+	}
+	
+	public Electrodomestico(double precioBase, Color mycolor, Consumo myconsumo, double peso) {
+		this.precioBase = precioBase;
+		this.mycolor = mycolor;
+		this.myconsumo = myconsumo;
+		this.peso = peso;
+	}
+
+	@Override
+	public String toString() {
+		return "Electrodomestico [precioBase=" + precioBase + ", mycolor=" + mycolor + ", myconsumo=" + myconsumo
+				+ ", peso=" + peso + "]";
+	}
+	
+	
+}
---
+++ UD08-Ejercicio_3/src/ejercicio_3/MainApp.java
@@ -0,0 +1,25 @@
+package ejercicio_3;
+
+import ejercicio_3.Electrodomestico.Color;
+import ejercicio_3.Electrodomestico.Consumo;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		// Instancia al enum Color para acceder a los colores
+		Color miColor =  Color.rojo;
+		// Instancia al enum Consumo para acceder a los consumos
+		Consumo miConsumo = Consumo.D;
+		
+		Electrodomestico lavadora = new Electrodomestico();
+		Electrodomestico microondas = new Electrodomestico(150,30);
+		Electrodomestico nevera = new Electrodomestico(200, miColor,miConsumo,150);
+
+		/* Imprimos los objetos de la clase electrodomestico mediante toString */
+		System.out.println(lavadora);
+		System.out.println(microondas);
+		System.out.println(nevera);
+	}
+
+}
